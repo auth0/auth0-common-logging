be8ae813a6b50c56cefe2160c0ecc76c7c7048cb
José F. Romaniello <jfromaniello@gmail.com>
2015-09-16 08:57:26 -0300

    minor

--- eventlogger/server.js
+++ eventlogger/server.js
@@ -1,11 +1,12 @@
 module.exports.watch = function (logger, server) {
   server.on('listening', function () {
     var address = this.address();
+    var port = typeof address === 'string' ? address : address.port;
 
     logger.info({
       log_type: 'listening',
-      address: address
-    },'listening on ' + address.port);
+      port: port
+    },'listening on ' + port);
 
   });
 
