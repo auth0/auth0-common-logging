9d21dee70594531616e8fabb9b48fed7e2219162
José F. Romaniello <jfromaniello@gmail.com>
2015-09-25 16:34:45 -0300

    add hapi_server event logger

---
+++ eventlogger/hapi_server.js
@@ -0,0 +1,41 @@
+module.exports.watch = function  (logger, server) {
+  server
+  .on('request-internal', function (request, data, tags) {
+    if (tags.received) {
+      return logger.debug({
+        log_type: 'request',
+        req: request
+      });
+    }
+    if (tags.response && request.path !== '/api/v2/test') {
+      const log_event = {
+        log_type: 'response',
+        req:  request,
+        res:  request.response
+      };
+      if (tags.closed && tags.error) {
+        log_event.err = new Error('connection closed');
+      }
+      if (tags.aborted && tags.error) {
+        log_event.err = new Error('connection aborted');
+      }
+      return logger.info(log_event);
+    }
+  }).on('request', function (request, data, tags) {
+    const level = tags.debug ? 'debug': 'info';
+    logger[level]({ req: request, data: data.data });
+  }).on('request-error', function (request, error) {
+    logger.error({
+      log_type: 'request_error',
+      req: request,
+      res: request.response,
+      err: error,
+    });
+  }).on('log', function (event, tags) {
+    logger.info({
+      log_type: 'server_log',
+      data: event,
+      tags: tags
+    });
+  });
+};
--- eventlogger/index.js
+++ eventlogger/index.js
@@ -1,6 +1,7 @@
 var process_watcher = require('./process');
 var server_watcher = require('./server');
 var process_info = require('../lib/process_info');
+var hapi_server = require('./hapi_server');
 
 function EventLogger (logger) {
   this.logger =  process.env.NODE_ENV === 'production' ?
@@ -15,6 +16,11 @@ EventLogger.prototype.watch = function (obj) {
 
   if (obj.constructor.name === 'Server') {
     return server_watcher.watch(this.logger, obj);
+  }
+
+  //stupid way of identify a hapi server
+  if (obj.app && obj.info && obj.info.id && obj.info.created) {
+    return hapi_server.watch(this.logger, obj);
   }
 
   throw new Error('unknown object type');
