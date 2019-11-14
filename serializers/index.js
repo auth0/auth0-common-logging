86d85f2168b635bd1f5eb4bd47bfd1099d1d00db
Nathan White <nathan.white@auth0.com>
2019-11-14 06:51:30 -0600

    fix(serializer): express get resp header

--- serializers/index.js
+++ serializers/index.js
@@ -38,7 +38,7 @@ var common_serializers = {
   },
   res: function (res) {
     if (!res) return {};
-    var headers = res.headers || {};
+    var headers = (typeof res.getHeaders === 'function') ? res.getHeaders() : headers || {};
     var result = {
       statusCode: res.statusCode,
       headers:    {
